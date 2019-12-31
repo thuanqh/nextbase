c13616c981f9fcdfa0131d61227588e82ec7c73a
Jason Quach <thuanqh@gmail.com>
2019-12-31 11:15:32 +0700

    Refactor project structure

--- src/server.js
+++ src/server.js
@@ -9,18 +9,6 @@ const handle = app.getRequestHandler();
 app.prepare().then(() => {
   const server = express();
 
-  server.get("/a", (req, res) => {
-    return app.render(req, res, "/a", req.query);
-  });
-
-  server.get("/b", (req, res) => {
-    return app.render(req, res, "/b", req.query);
-  });
-
-  server.get("/posts/:id", (req, res) => {
-    return app.render(req, res, "/posts", { id: req.params.id });
-  });
-
   server.all("*", (req, res) => {
     return handle(req, res);
   });
