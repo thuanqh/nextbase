762200a54b28265abd54f6aa391bcfa04732731c
Jason Quach <thuanqh@gmail.com>
2021-04-06 10:26:03 +0700

    Update package and next-pwa

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 const withPWA = require("next-pwa");
-const runtimeCaching = require("next-pwa/cache");
 
 module.exports = withPWA({
+  future: {
+    webpack5: true,
+  },
   pwa: {
     dest: "public",
-    runtimeCaching,
   },
 });
--- pages/_document.js
+++ pages/_document.js
@@ -3,13 +3,32 @@ import Document, { Html, Head, Main, NextScript } from "next/document";
 import { ServerStyleSheets } from "@material-ui/core/styles";
 import theme from "../utils/theme";
 
+const APP_NAME = "NextBase";
+const APP_DESCRIPTION = "This is an next base starter";
+
 export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
         <Head>
-          {/* PWA primary color */}
+          <meta name="application-name" content={APP_NAME} />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta
+            name="apple-mobile-web-app-status-bar-style"
+            content="default"
+          />
+          <meta name="apple-mobile-web-app-title" content={APP_NAME} />
+          <meta name="description" content={APP_DESCRIPTION} />
+          <meta name="format-detection" content="telephone=no" />
+          <meta name="mobile-web-app-capable" content="yes" />
           <meta name="theme-color" content={theme.palette.primary.main} />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/icons/apple-touch-icon.png"
+          />
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="shortcut icon" href="icons/favicon.ico" />
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
