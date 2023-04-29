901a9448cb1da68c1e31982a384d84ad2d8d3575
Jason Quach <thuanqh@gmail.com>
2023-04-29 11:45:30 +0700

    Use Prisma and Next Image in React Server Component

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,12 @@ import Link from "next/link"
 
 import { siteConfig } from "@/config/site"
 import { buttonVariants } from "@/components/ui/button"
+import { prisma } from "@/lib/prisma"
+import Image from "next/image"
 
-export default function IndexPage() {
+export default async function IndexPage() {
+  let users = await prisma.user.findMany()
+
   return (
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
       <div className="flex max-w-[980px] flex-col items-start gap-2">
@@ -33,6 +37,21 @@ export default function IndexPage() {
         >
           GitHub
         </Link>
+      </div>
+      <div>
+        {users.map(user => (
+          <div
+            key={user.id}
+          >
+            <Image
+              src={`https://robohash.org/${user.id}?set=set2&size=180x180`}
+              alt={user.name}
+              width={180}
+              height={180}
+            />
+            <h3>{user.name}</h3>
+          </div>
+        ))}
       </div>
     </section>
   )
